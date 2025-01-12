6892bc24c9b3fac8353c052a2d8bb9ef128cfab5
Akshay <akshaysebastian087@gmail.com>
2025-01-12 23:17:29 +0530

    investment_report

---
+++ roi.py
@@ -0,0 +1,6 @@
+
+
+investment = float(input("Enter the amount of the investment: "))
+return_amount = float(input("Enter the amount of the return: "))
+roi = (return_amount - investment) / investment
+print("The ROI is", roi)
