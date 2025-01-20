cff3728f6ad2bf84361c810e4f42799ee47dc8e4
Akshay <akshaysebastian087@gmail.com>
2025-01-20 11:33:41 +0530

    square_root

--- roi.py
+++ roi.py
@@ -1,6 +1,21 @@
 
 
-investment = float(input("Enter the amount of the investment: "))
-return_amount = float(input("Enter the amount of the return: "))
-roi = (return_amount - investment) / investment
-print("The ROI is", roi)
+intial_amount = float(input("Enter the initial amount: "))
+interest_rate = float(input("Enter the interest rate: "))
+years = int(input("Enter the number of years: "))
+
+init = intial_amount
+final_amount = intial_amount
+interest_rate = interest_rate / 100
+
+print("Year  \tIntial_Amount  \tInterest  \tFinal_Amount")    
+for i in range(1,years+1):
+    
+
+    final_amount+=round(interest_rate*intial_amount, 2)
+   
+    print(i, "\t",intial_amount, "\t",interest_rate, "\t\t",final_amount)
+    intial_amount = final_amount
+print()
+print("The final amount after", years, "years is", final_amount)
+print("The interest earned after", years, "years is", round(final_amount-init,2))
